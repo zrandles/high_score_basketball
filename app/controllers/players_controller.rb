b600e009f26e605d289c7af62a44cdbd663dc2de
zrandles <zrandles@gmail.com>
2025-10-20 12:55:19 -0700

    Fix season filter - use 2024-25 season data

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,18 +2,18 @@ class PlayersController < ApplicationController
   def index
     @players = Player.includes(:player_summary)
                      .joins(:player_summary)
-                     .where(player_summaries: { season: '2023-24' })
+                     .where(player_summaries: { season: '2024-25' })
                      .order('player_summaries.avg_weekly_high DESC')
   end
 
   def show
     @player = Player.includes(:player_summary, :weekly_highs).find(params[:id])
-    @weekly_highs = @player.weekly_highs.where(season: '2023-24').order(:week_number)
+    @weekly_highs = @player.weekly_highs.where(season: '2024-25').order(:week_number)
     @summary = @player.player_summary
   end
 
   def raw_data
     @player = Player.includes(:game_logs).find(params[:id])
-    @game_logs = @player.game_logs.where(season: '2023-24').order(:game_date)
+    @game_logs = @player.game_logs.where(season: '2024-25').order(:game_date)
   end
 end
